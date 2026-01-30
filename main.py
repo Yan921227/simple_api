dd60f9cb032cb0070ff2edc3dd8634ce85aae377
Yan921227 <125242772+Yan921227@users.noreply.github.com>
2026-01-30 23:26:58 +0800

    get and post 初步應用

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+items = []
+
+# 用來描述「前端會送來什麼資料」 前端會送一個叫 name 的文字過來
+class ItemCreate(BaseModel):
+    name: str
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello, Simple API"}
+
+
+@app.post("/items")
+def create_item(item: ItemCreate):
+    items.append(item)   # ✅ 把資料存進清單
+    return {
+        "you_sent": item
+    }
+
+@app.get("/items")
+def list_items():
+    return items
